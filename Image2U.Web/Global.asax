8926258b96dbc793b15588ad3c8bf2474b16e914
Andy Chang <andyta24@gmail.com>
2021-01-26 11:08:54 +0800

    加入專案檔案。

---
+++ Image2U.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Image2U.Web.MvcApplication" Language="C#" %>
---
+++ Image2U.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Image2U.Web.Startup))]
+namespace Image2U.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
